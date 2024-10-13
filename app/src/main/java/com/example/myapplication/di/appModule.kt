28109fae4291ccdfd1fba5b905204d9ab1f1d473
Artem Ocheretny <artsyom.ocheretny@daftmobile.com>
2024-10-13 15:48:10 +0200

    Add receiver in manifestUpdate koin modules

--- app/src/main/java/com/example/myapplication/di/appModule.kt
+++ app/src/main/java/com/example/myapplication/di/appModule.kt
@@ -1,6 +1,10 @@
 package com.example.myapplication.di
 
+import android.content.Context
+import androidx.core.app.NotificationManagerCompat
 import androidx.room.Room
+import androidx.work.WorkerParameters
+import com.example.bootcounter.NotificationWorker
 import com.example.myapplication.data.BootRecordsDatabase
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
@@ -9,5 +13,7 @@ val appModule = module {
     single {
         Room.databaseBuilder(androidContext(), BootRecordsDatabase::class.java, "boot-records-database").build()
     }
-    single { get<BootRecordsDatabase>().userDao() }
+    single { NotificationManagerCompat.from(get()) }
+    single { get<BootRecordsDatabase>().bootRecordDao() }
+    factory { (appContext: Context, workerParams: WorkerParameters) -> NotificationWorker(appContext, workerParams) }
 }
